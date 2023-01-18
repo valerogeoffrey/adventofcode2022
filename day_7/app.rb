dd5ecb2750086dd64288ac1a14089cb683ec5c79
Geoffrey <geoffrey.valero@hivency.com>
2023-01-18 20:12:39 +0100

    day_7

--- day_7/app.rb
+++ day_7/app.rb
@@ -2,38 +2,94 @@ require_relative 'input'
 require 'ostruct'
 
 def sys(input = nil)
-  # take account
-  #  dir X
-  # cd /
-  # cd ..
-  file = File.open("input_test.txt")
-  folder = {}
-  folder[:root] = {}
-  cursor = folder[:root]
+  file = File.open("input.txt")
 
-  file.readlines.map(&:chomp).each_with_index do |line, idx|
-    if line == '$ cd /'
-      cursor = folder[:root]
+  cd_x = []
+  dir_x = []
+  cursor = Folder.new(root: true, name: 'root')
+  file.readlines.map(&:chomp).each do |line|
+
+    cursor = cursor.go_back_to_root if line == '$ cd /'
+
+    if line.include?("$ cd") && line != '$ cd /'
+      if line[5 .. line.size] == '..'
+        cursor = cursor.previous_folder
+      else
+        cd_x << line[5 .. line.size]
+        cursor = cursor.go_in(line[5 .. line.size])
+      end
     end
 
-    if line.include?( "$ cd") && line != '$ cd /'
-      pp line[5..line.size]
-      cursor = cursor[]
+    if line.include?("dir")
+      dir_x << line[4 .. line.size]
+      folder = Folder.new(name: line[4 .. line.size])
+      cursor.add_folder(folder)
     end
+
+    if line[0].numeric?
+      ff = FileS.new(line.split(' ').last, line.split(' ').first)
+      cursor.add_files(ff)
+    end
+  end
+
+  pp 'dir_x.size'
+  pp dir_x.size
+  pp cd_x.size
+  data = {}
+  cursor = cursor.go_back_to_root
+
+  pp "start"
+  pp "DATA --> #{data}"
+  pp "in cursor -> #{cursor.name} // prev --> #{cursor.previous_folder?} // has_previous? #{cursor.previous_folder?}"
+  data['rrr'] = cursor.files_size.sum
+
+  tt = dig_folder(cursor, data)
+  tt.delete('root')
+  pp tt
+  pp tt.values.select{|v| v < 100_000}.sum
+end
+
+def dig_folder(cursor, data)
+  cursor.folders.each do |cursor|
+    data[cursor.path_name] = 0 if data[cursor.path_name].nil?
+    data[cursor.previous.path_name] = 0 if data[cursor.previous.path_name].nil?
+    data[cursor.path_name] = data[cursor.path_name] + cursor.files_size.sum
+    data['rrr'] = data['rrr'] + cursor.files_size.sum
+
+    if cursor.previous_folder? && cursor.previous.path_name != 'root'
+      data[cursor.previous.path_name] = data[cursor.previous.path_name] + data[cursor.path_name]
+    end
+
+    return data unless cursor.folders?
+
+    dig_folder(cursor, data)
+  end
+  data
+end
+
+def sys_2(input = nil) end
+
+class String
+  def numeric?
+    Float(self) != nil rescue false
   end
 end
 
-def sys_2(input = nil)
-end
-
 class Folder
-  attr_accessor :root, :previous, :name, :files, :folders
-  def initialize(root: false, name: )
+  attr_accessor :root, :previous, :name, :files, :folders, :end, :path_name
+
+  def initialize(root: false, name:)
     @root = root
-    @name  = name
+    @name = name
     @folders = []
     @files = []
     @previous = root ? :root : nil
+    @path_name = root ? 'root' : nil
+    @end = true
+  end
+
+  def previous_path_name
+    previous? ? previous_folder.path_name : path_name
   end
 
   def add_files(files)
@@ -41,69 +97,122 @@ class Folder
   end
 
   def add_folder(folder)
-    pp folder.name
-    pp self.name
     @folders << folder
-    @previous = self
+    @end = false
+    folder.previous = self
+    folder.path_name = "#{path_name}/#{folder.name}"
   end
 
   def next
     return @folders.first if @folders.size == 1
 
-    :end
+    self
   end
 
   def go_in(name)
     cursor = nil
-      @folders.each do |node|
+    @folders.each do |node|
       if node.name == name
         cursor = node
         break
       end
 
-      :not_exist
+      self
     end
     cursor
   end
 
-  def previous_folder
-    pp self&.name
-    if @root
-      :you_are_in_the_root_folder
+  def go_back_to_root
+    cursor = self
+    if root == false
+      cursor = previous_folder
+      cursor.go_back_to_root
     else
-      @previous
+      cursor
     end
+  end
+
+  def list_files
+    files.map(&:to_s)
+  end
+
+  def list_folders
+    folders.map(&:name)
+  end
+
+  def info
+    pp name
+    pp list_files
+    pp list_folders
+  end
+
+  def previous_folder
+    if root == true
+      self
+    else
+      previous
+    end
+  end
+
+  def previous_folder?
+    @root == false
+  end
+
+  def files?
+    files.size > 0
+  end
+
+  def folders?
+    folders.size > 0
+  end
+
+  def files_size
+    files.map(&:size)
   end
 end
 
 class FileS
   attr_accessor :name, :sizef
+
   def initialize(name, sizef)
     @name = name
     @sizef = sizef
   end
+
+  def to_s
+    { name: name, size: sizef }
+  end
+
+  def size
+    sizef.to_i
+  end
 end
 
-root = Folder.new(root: true, name:'root')
-i = FileS.new('i', 584)
-b = FileS.new('b', 29116)
-c = FileS.new('c', 2557)
-root.add_files(i)
-root.add_files(b)
-root.add_files(c)
+pp 'sys'
+sys
+pp 'sys_response'
+#sys_response
 
-a = Folder.new( name:'a')
-e = Folder.new( name:'e')
+root = Folder.new(root: true, name: 'root')
+a = Folder.new(name: 'a')
+e = Folder.new(name: 'e')
+x = Folder.new(name: 'x')
+z = Folder.new(name: 'z')
 
 root.add_folder(a)
 a.add_folder(e)
+e.add_folder(x)
+x.add_folder(z)
 
-e = root.go_in('a').go_in('e')
-root = e.previous_folder
+cursor = z
 
-pp e.name
-pp root.inspect
-raise
+pp cursor.name
+cursor = cursor.go_back_to_root
+pp cursor.name
+
+#pp a.previous_folder
+raise('END')
+
 puts root.files.map(&:sizef).sum
 puts root.folders.map(&:name)
 puts root.files
